8af3b907992f058c8b04e84466945f7131f36dcd
Helios He <helioshe4@gmail.com>
2023-05-23 22:03:19 -0400

    multi page palceholder

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+//import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Layout />}> */}
+          <Route
+            index
+            element={
+              <>
+                <Layout />
+                <Home />
+              </>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/NoPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoPage() {
+  return <h1>this is the no page error page</h1>;
+}
+
+export default NoPage;
