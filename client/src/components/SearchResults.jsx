f43996b1fe6a02476351b32de5f65d9ce7cd6bb3
Helios He <helioshe4@gmail.com>
2023-06-12 21:36:17 -0400

    added search filter functionality

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Table from "react-bootstrap/esm/Table";
 
+import "./stylingComponents/SearchResults.css";
+
 const SearchResults = ({ searchQuery, selectedCheckboxes }) => {
   const [skaters, setSkaters] = useState([]);
+  const [filteredSkaters, setFilteredSkaters] = useState([]);
 
   useEffect(() => {
     const getSkaters = async () => {
@@ -17,23 +20,59 @@ const SearchResults = ({ searchQuery, selectedCheckboxes }) => {
     getSkaters();
   }, []);
 
+  useEffect(() => {
+    const filterSkaters = async () => {
+      try {
+        const sortedSkaters = skaters
+          .filter((skater) =>
+            skater.skater_name.toLowerCase().includes(searchQuery.toLowerCase())
+          )
+          .sort((a, b) => {
+            // Handle empty or null country values
+            const countryA = a.country ? a.country.trim() : "";
+            const countryB = b.country ? b.country.trim() : "";
+
+            if (countryA === "" && countryB === "") {
+              return a.skater_name.localeCompare(b.skater_name); // Sort by name if both countries are empty or null
+            }
+            if (countryA === "") return 1; // Move a to the end if its country is empty or null
+            if (countryB === "") return -1; // Move b to the end if its country is empty or null
+
+            // Sort by country, then by name
+            if (countryA !== countryB) {
+              return countryA.localeCompare(countryB); // Sort by country if they are different
+            }
+            return a.skater_name.localeCompare(b.skater_name); // Sort by name if countries are the same
+          });
+
+        for (let i = 0; i < sortedSkaters.length; i++) {
+          console.log(sortedSkaters[i].skater_name);
+        }
+        setFilteredSkaters(sortedSkaters);
+      } catch (err) {
+        console.error(`Error fetching skaters: ${err.message}`);
+      }
+    };
+    filterSkaters();
+  }, [searchQuery, skaters]);
+
   return (
     <>
-      <Table>
+      <Table borderless className="search-result-table">
         <thead>
           <tr>
             <th>Name</th>
             <th>Country</th>
-            <th>Age</th>
+            <th>DOB</th>
             {/* Add more table headers as needed */}
           </tr>
         </thead>
         <tbody>
-          {skaters.map((skater) => (
-            <tr key={skater.id}>
-              <td>{skater.name}</td>
+          {filteredSkaters.map((skater) => (
+            <tr key={skater.skater_id}>
+              <td>{skater.skater_name}</td>
               <td>{skater.country}</td>
-              <td>{skater.age}</td>
+              <td>{skater.dob ? skater.dob.substring(0, 10) : ""}</td>
               {/* Add more table cells as needed */}
             </tr>
           ))}
